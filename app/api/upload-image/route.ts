9175fcd4136d25e7f306df4d3af28ef6d5492e26
Tahir Khan <tahirtrk42@gmail.com>
2025-03-22 17:13:47 +0530

    prisma, neonDB setup, clerk for authentication, setting up middleware for protected route and then writing code to upload any file on the cloudinary

---
+++ app/api/upload-image/route.ts
@@ -0,0 +1,62 @@
+import { auth } from "@clerk/nextjs/server";
+import { v2 as cloudinary } from "cloudinary";
+import { NextRequest, NextResponse } from "next/server";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+interface CloudinaryUploadResult {
+  public_id: string;
+  [key: string]: any;
+}
+
+export async function POST(request: NextRequest) {
+  // Check first user is authorized or not
+  const authData = auth();
+  const userId = (await authData).userId;
+
+  if (!userId) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  try {
+    // Extract file from the frontend
+    const formData = await request.formData();
+    const file = formData.get("file") as File | null;
+
+    if (!file) {
+      return NextResponse.json({ error: "File not found" }, { status: 400 });
+    }
+
+    //a low-level binary representation of file
+    const byte = await file.arrayBuffer();
+
+    // then it coverts into node.js buffer, which is easier to work in backend
+    const buffer = Buffer.from(byte);
+
+    const result = await new Promise<CloudinaryUploadResult>(
+      (resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+          { folder: "next-cloudinary-uploads" },
+          (error, result) => {
+            if (error) reject(error);
+            else resolve(result as CloudinaryUploadResult);
+          }
+        );
+        uploadStream.end(buffer);
+      }
+    );
+
+    return NextResponse.json({ publicId: result.public_id }, { status: 200 });
+  } catch (error) {
+    console.log("Image upload failed: ", error);
+    return NextResponse.json(
+      { error: "Error occured while uploading the images" },
+      { status: 401 }
+    );
+  }
+}
---
+++ app/api/upload-video/route.ts
@@ -0,0 +1,96 @@
+import { auth } from "@clerk/nextjs/server";
+import { PrismaClient } from "@prisma/client";
+import { v2 as cloudinary } from "cloudinary";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+interface CloudinaryUploadResult {
+  public_id: string;
+  bytes: number;
+  duration?: number;
+  [key: string]: any;
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    // Check first user is authorized or not
+    const authData = auth();
+    const userId = (await authData).userId;
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    // More strict validation
+    if (
+      !process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME ||
+      !process.env.CLOUDINARY_API_KEY ||
+      !process.env.CLOUDINARY_API_SECRET
+    ) {
+      return NextResponse.json(
+        { error: "Cloudinary credentials not found" },
+        { status: 500 }
+      );
+    }
+
+    // Extract file from the frontend
+    const formData = await request.formData();
+    const file = formData.get("file") as File | null;
+    const title = formData.get("title") as String;
+    const description = formData.get("description") as String;
+    const originalSize = formData.get("originalSize") as String;
+
+    if (!file) {
+      return NextResponse.json({ error: "File not found" }, { status: 400 });
+    }
+
+    //a low-level binary representation of file
+    const byte = await file.arrayBuffer();
+
+    // then it coverts into node.js buffer, which is easier to work in backend
+    const buffer = Buffer.from(byte);
+
+    const result = await new Promise<CloudinaryUploadResult>(
+      (resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+          {
+            resource_type: "video",
+            folder: "next-video-uploads",
+            transformation: [{ quality: "auto", fetch_format: "mp4" }],
+          },
+          (error, result) => {
+            if (error) reject(error);
+            else resolve(result as CloudinaryUploadResult);
+          }
+        );
+        uploadStream.end(buffer);
+      }
+    );
+
+    const video = await prisma.video.create({
+      data: {
+        title: String(title),
+        description: String(description),
+        publicId: result.public_id,
+        originalSize: String(originalSize),
+        compressedSize: String(result.bytes),
+        duration: result.duration || 0,
+      },
+    });
+
+    return NextResponse.json(video);
+  } catch (error) {
+    console.log("video upload failed: ", error);
+    return NextResponse.json(
+      { error: "Error occured while uploading the video" },
+      { status: 401 }
+    );
+  }
+}
